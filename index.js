0a1d5f4b7aaf80388a40994f88e12c7a363e88c0
Harshit Singh <130093039+harshit-1245@users.noreply.github.com>
2023-09-19 18:02:28 +0000

    day-1

---
+++ index.js
@@ -0,0 +1,10 @@
+// function decleration without parameter
+
+function square(){
+    let a =2;
+   let sq=a*a;
+   console.log(sq);
+    
+ }
+ square() // calling function  by its square and with parenthsis.
+ 
