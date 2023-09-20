3ab23f5d937e2c92fbd04807cece4bc1b6fd1d69
Harshit Singh <130093039+harshit-1245@users.noreply.github.com>
2023-09-20 17:56:19 +0000

    day2

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ function square(){
  }
  square() // calling function  by its square and with parenthsis.
  
+ //function returning a value
+
+ function sum1(){
+  let a=5;
+  let b=6;
+  let c = a+b;
+  return c;
+ }
+
+ console.log(sum1())
+
+ //function with a parameter
+ function sum(r){
+  return Math.PI*r*r;
+ }
+ console.log(sum(5))
