4e7259bcb378f1ece0f8c0965fa5a0f17b0d6856
Harshit Singh <130093039+harshit-1245@users.noreply.github.com>
2023-09-24 13:52:11 +0000

    half complete

--- index.js
+++ index.js
@@ -24,3 +24,62 @@ function square(){
   return Math.PI*r*r;
  }
  console.log(sum(5))
+
+ //function with multiple parameter
+
+function ArraySum(arr){
+  let sum=0;
+  for(let i=0;i<arr.length;i++){
+    sum +=arr[i];
+  }
+  return sum;
+}
+const numbers=[1,2,3,4,5];
+console.log(ArraySum(numbers));
+
+const areaOfCircle=(radius)=>{
+  let area=Math.floor(Math.PI*(radius*radius));
+  return area;
+}
+
+let n=10;
+console.log(areaOfCircle(n));
+
+function sumArray(){
+  
+  let sum=0;
+  for(let i=0;i<arguments.length;i++){
+    sum +=arguments[i];
+  }
+  return sum;
+  
+}
+console.log(sumArray(1,2,3,4,5))
+
+const sumArr=(...arr)=>{
+  let sum=0;
+  for(let i=0;i<arr.length;i++){
+    sum +=arr[i];
+  }
+  return sum;
+}
+
+console.log(sumArr(1,2,3,4))
+
+//Anonymous Function
+
+const anonymousFun=function(){
+  console.log("Anonymous Function here")
+}
+//expression function
+
+const Square=function(n){
+  return n*n;
+}
+console.log(Square(2))
+
+//Self Invoking Functions
+
+(function(n) {
+  console.log(n * n)
+})(5)
